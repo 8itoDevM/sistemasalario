2b5bdc018e6097072a200d0b147c9dac1d60a791
8itoDevM <gustavomarcolino862@gmail.com>
2024-04-19 22:24:29 -0300

    Trying to make it work

--- assets/main.js
+++ assets/main.js
@@ -19,11 +19,82 @@ fetch(full_url)
     sendButton = document.getElementById("send-btn");
     sendButton.style.visibility = 'hidden'
 
-    let searchValue = document.getElementById('text-search').value;
+    let hourText = document.createElement("p");
+    var userLastRow = 0;
 
     setInterval(function() {
-        
+        var searchValue = document.getElementById('text-search').value;
 
-        //console.log(searchValue);
+        for(let i = data.table.rows.length - 1; i >= 0; i--){
+            if(searchValue == data.table.rows[i].c[3].v && data.table.rows[i].c[4].v) {
+                getUserLastRow();
+                hourText.innerHTML = "Horas trabalhadas: " + data.table.rows[userLastRow].c[5].v / 1000;
+
+                calculateSalary();
+            }
+        }
+
+        function getUserLastRow(){
+            for(let i = data.table.rows.length - 1; i >= 0; i--){
+                if(searchValue == data.table.rows[i].c[3].v && data.table.rows[i].c[4].v == "Fechado"){
+                   userLastRow = i;
+                   //return userLastRow;
+                //    console.log(data.table.rows[i].c[2].f);
+                //    console.log(data.table.rows[i].c[4].v);
+                //    console.log(data.table.rows[i].c[3].v);
+                //    console.log(" ");
+                //    console.log(" ");
+                   break;
+                }
+            } 
+        }
+        function calculateSalary(){
+            var monthlyHours = 0;
+            var oneMonthAgo = new Date();
+            oneMonthAgo.setDate(oneMonthAgo.getDate() - 30);
+
+            console.log("Mês passado: "+ oneMonthAgo);
+
+            getUserLastRow();
+            for(let i = userLastRow; i >= 1; i--){
+                console.log("Indice do for: "+ i);
+
+                var pointDate = new Date();
+
+                strDate = data.table.rows[i].c[2].f;
+                pointDate.setDate(strDate.slice(0, 2), strDate.slice(2, 4))
+                console.log("Point date: "+ pointDate);
+
+                if(pointDate.getMonth() < oneMonthAgo.getMonth()){
+                    break;
+                }
+
+                // checar se o nome é igual
+                if(data.table.rows[i].c[3].v == searchValue){
+                    continue;
+                }
+                console.log("Nome que sai na checagem: "+ data.table.rows[i].c[3].v);
+
+
+                var startTime = new Date(data.table.rows[i].c[0].f);
+
+                console.log("Start time: "+ startTime);
+
+                var endTime = new Date(data.table.rows[i].c[1].f);
+
+                console.log("End time: "+ endTime);
+
+                if (isNaN(startTime) || isNaN(endTime)) {
+                    continue;
+                }
+                
+                var hoursWorked = (endTime.getTime() - startTime.getTime()) / 1000 / 60 / 60;
+                monthlyHours += hoursWorked;
+            }
+
+            sendButton.style.visibility = 'visible';
+        }
     }, 500);
+
+    document.getElementById("information").appendChild(hourText);
 }); 
