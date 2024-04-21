23510de2c3f94ae4ed45f5dd3567e20cc1065f4a
8ito <gustavomarcolino862@gmail.com>
2024-04-21 16:00:13 -0300

    Now the code is functional

--- assets/main.js
+++ assets/main.js
@@ -25,19 +25,116 @@ fetch(full_url)
     setInterval(function() {
         var searchValue = document.getElementById('text-search').value;
 
-        for(let i = data.table.rows.length - 1; i >= 0; i--){
-            if(searchValue == data.table.rows[i].c[3].v && data.table.rows[i].c[4].v) {
-                getUserLastRow();
-                hourText.innerHTML = "Horas trabalhadas: " + data.table.rows[userLastRow].c[5].v / 1000;
+        var monthlyHours = 0;
+        var oneMonthAgo = new Date();
+        oneMonthAgo.setDate(oneMonthAgo.getDate() - 30);
 
-                calculateSalary();
+        for(let i = data.table.rows.length - 1; i >= 0; i--){
+            if(searchValue == data.table.rows[i].c[3].v && data.table.rows[i].c[4].v == "Fechado") {
+                getUserLastRow();
+
+                console.log("Nome da pessoa do userlastrow: " + data.table.rows[i].c[3].v);
+                console.log("Data deste ponto do user last row: " + data.table.rows[i].c[2].f);
+                
+                console.log(" ");
+
+                var pointDate = new Date();
+
+                strDate = data.table.rows[i].c[2].f;
+                pointDate.setDate(strDate.slice(0, 2), strDate.slice(2, 4))
+                console.log("Point date: "+ pointDate);
+
+                if (pointDate < oneMonthAgo) {
+                    console.log("pointdate é menor que onemonthago")
+                    break; // Se a data do ponto é mais de uma semana atrás, paramos de iterar
+                }
+                
+                // Verifique se o nome do usuário é o mesmo
+                if (data.table.rows[i].c[3].v != searchValue) {
+                    console.log("o nome não é igual");
+                    continue; // Se o nome do usuário não é o mesmo, ignoramos este ponto
+                }
+                
+                console.log(" ");
+
+                console.log("Tempo que irá ser inserido em start time: " + data.table.rows[i].c[0].f);
+                console.log("Tempo que irá ser inserido em end time: " + data.table.rows[i].c[1].f);
+
+                var startTime = new Date();
+                var strStartTime = data.table.rows[i].c[0].f;
+
+                // Verifique se a string contém uma barra (":")
+                if (strStartTime.includes(":")) {
+                    // Se sim, divida a string em horas e minutos
+                    var timeStartParts = strStartTime.split(":");
+                    startTime.setHours(timeStartParts[0]);
+                    startTime.setMinutes(timeStartParts[1]);
+                } else {
+                    // Se não, continue usando o método slice
+                    startTime.setHours(strStartTime.slice(0, 2));
+                    startTime.setMinutes(strStartTime.slice(2, 4));
+                }
+
+                var endTime = new Date();
+                var strEndTime = data.table.rows[i].c[1].f;
+
+                // Verifique se a string contém uma barra (":")
+                if (strEndTime.includes(":")) {
+                    // Se sim, divida a string em horas e minutos
+                    var timeEndParts = strEndTime.split(":");
+                    endTime.setHours(timeEndParts[0]);
+                    endTime.setMinutes(timeEndParts[1]);
+                } else {
+                    // Se não, continue usando o método slice
+                    endTime.setHours(strEndTime.slice(0, 2));
+                    endTime.setMinutes(strEndTime.slice(2, 4));
+                }
+
+                console.log(" ");
+
+                console.log("Start time do user last row: " + startTime);
+                console.log("End time do user last row: " + endTime);
+
+                console.log(" ");
+
+                if (isNaN(startTime) || isNaN(endTime)) {
+                    continue; // Se não pudermos converter a hora de início ou fim para uma data, ignoramos este ponto
+                }
+                
+                var hoursWorked = (endTime.getTime() - startTime.getTime()) / 1000 / 60 / 60; // Convert milissegundos para horas
+                monthlyHours += hoursWorked;
+
+                console.log("Horas trabalhadas no mês: " + monthlyHours + "Horas trabalhadas para ser adicionadas nisto: " + hoursWorked);
+
+                hourText.innerHTML = "Horas trabalhadas neste mês: " + monthlyHours.toFixed(2);
+
+                console.log(" ");
+                console.log(" ");
+                console.log(" ");
+
+                sendButton.style.visibility = 'visible';
             }
+        }
+
+        function calculateSalary(){
+            var monthlyHours = 0;
+            var oneMonthAgo = new Date();
+            oneMonthAgo.setDate(oneMonthAgo.getDate() - 30);
+
+            //console.log("Mês passado: "+ oneMonthAgo);
+            console.log(userLastRow);
+
+            for(let i = userLastRow; i >= 1; i--){
+                
+            }
+
+            
         }
 
         function getUserLastRow(){
             for(let i = data.table.rows.length - 1; i >= 0; i--){
-                if(searchValue == data.table.rows[i].c[3].v && data.table.rows[i].c[4].v == "Fechado"){
-                   userLastRow = i;
+                if(searchValue == data.table.rows[i].c[3].v){
+                    userLastRow = i;
                    //return userLastRow;
                 //    console.log(data.table.rows[i].c[2].f);
                 //    console.log(data.table.rows[i].c[4].v);
@@ -47,52 +144,6 @@ fetch(full_url)
                    break;
                 }
             } 
-        }
-        function calculateSalary(){
-            var monthlyHours = 0;
-            var oneMonthAgo = new Date();
-            oneMonthAgo.setDate(oneMonthAgo.getDate() - 30);
-
-            console.log("Mês passado: "+ oneMonthAgo);
-
-            getUserLastRow();
-            for(let i = userLastRow; i >= 1; i--){
-                console.log("Indice do for: "+ i);
-
-                var pointDate = new Date();
-
-                strDate = data.table.rows[i].c[2].f;
-                pointDate.setDate(strDate.slice(0, 2), strDate.slice(2, 4))
-                console.log("Point date: "+ pointDate);
-
-                if(pointDate.getMonth() < oneMonthAgo.getMonth()){
-                    break;
-                }
-
-                // checar se o nome é igual
-                if(data.table.rows[i].c[3].v == searchValue){
-                    continue;
-                }
-                console.log("Nome que sai na checagem: "+ data.table.rows[i].c[3].v);
-
-
-                var startTime = new Date(data.table.rows[i].c[0].f);
-
-                console.log("Start time: "+ startTime);
-
-                var endTime = new Date(data.table.rows[i].c[1].f);
-
-                console.log("End time: "+ endTime);
-
-                if (isNaN(startTime) || isNaN(endTime)) {
-                    continue;
-                }
-                
-                var hoursWorked = (endTime.getTime() - startTime.getTime()) / 1000 / 60 / 60;
-                monthlyHours += hoursWorked;
-            }
-
-            sendButton.style.visibility = 'visible';
         }
     }, 500);
 
